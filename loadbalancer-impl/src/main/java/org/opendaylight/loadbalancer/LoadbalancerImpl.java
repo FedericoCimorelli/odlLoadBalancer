04127a3ee64e6164e8fe8572859e9d6a2d40b5f6
Federico Cimorelli <cimorellifederico@gmail.com>
2015-12-10 23:45:55 +0100

    Implemented load balacer APIs

--- loadbalancer-impl/src/main/java/org/opendaylight/loadbalancer/LoadbalancerImpl.java
+++ loadbalancer-impl/src/main/java/org/opendaylight/loadbalancer/LoadbalancerImpl.java
@@ -6,16 +6,16 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-
 package org.opendaylight.loadbalancer;
 
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.SettableFuture;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -28,21 +28,24 @@ import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
-import org.opendaylight.openflowplugin.api.openflow.md.core.session.SessionContext;
-import org.opendaylight.openflowplugin.api.openflow.md.core.session.SessionManager;
-import org.opendaylight.openflowplugin.openflow.md.core.session.OFSessionUtil;
 import org.opendaylight.openflowplugin.openflow.md.util.RoleUtil;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.GetLoadbalancerStatusOutput;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.GetLoadbalancerStatusOutputBuilder;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.GetSwitchRoleInput;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.GetSwitchRoleOutput;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.GetSwitchRoleOutputBuilder;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.Loadbalancer;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.Loadbalancer.LoadbalancerStatus;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.LoadbalancerBuilder;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.LoadbalancerService;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.SetSwitchRoleInput;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.SetSwitchRoleOutput;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.SetSwitchRoleOutputBuilder;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.StartLoadbalancerInput;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.StartLoadbalancerOutput;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.StartLoadbalancerOutputBuilder;
 import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.StopLoadbalancerOutput;
+import org.opendaylight.yang.gen.v1.http.netconfcentral.org.ns.loadbalancer.rev150901.StopLoadbalancerOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorRemoved;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeConnectorUpdated;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeRemoved;
@@ -50,15 +53,11 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeUpd
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.OpendaylightInventoryListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -68,6 +67,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
         OpendaylightInventoryListener, PacketProcessingListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoadbalancerImpl.class);
+    private static String TAG = "LoadBalancer";
     private ProviderContext providerContext;
     private DataBroker dataBroker;
     private ListenerRegistration<DataChangeListener> dcReg;
@@ -81,7 +81,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
     public void close() throws Exception {
         dcReg.close();
         rpcReg.close();
-        LOG.info("Registrations closed");
+        LOG.info(TAG, "Registrations closed");
     }
 
 
@@ -96,7 +96,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
         rpcReg = session.addRpcImplementation(LoadbalancerService.class, this);
         initLoadbalancerOperational();
         initLoadbalancerConfiguration();
-        LOG.info("onSessionInitiated: initialization done");
+        LOG.info(TAG, "onSessionInitiated: initialization done");
     }
 
 
@@ -107,10 +107,10 @@ public class LoadbalancerImpl implements BindingAwareProvider,
         DataObject dataObject = change.getUpdatedSubtree();
         if (dataObject instanceof Loadbalancer) {
             Loadbalancer loadbalancer = (Loadbalancer) dataObject;
-            LOG.info("onDataChanged - new Loadbalancer config: {}",
+            LOG.info(TAG, "onDataChanged - new Loadbalancer config: {}",
                     loadbalancer);
         } else {
-            LOG.warn("onDataChanged - not instance of Loadbalancer {}",
+            LOG.warn(TAG, "onDataChanged - not instance of Loadbalancer {}",
                     dataObject);
         }
     }
@@ -120,7 +120,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
     @Override
     public void onNodeConnectorRemoved(NodeConnectorRemoved arg0) {
         String s = "LOADBALANCER" + arg0.toString();
-        LOG.info(s);
+        LOG.info(TAG, s);
     }
 
 
@@ -128,7 +128,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
     @Override
     public void onNodeConnectorUpdated(NodeConnectorUpdated arg0) {
         String s = "LOADBALANCER" + arg0.toString();
-        LOG.info(s);
+        LOG.info(TAG, s);
     }
 
 
@@ -136,7 +136,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
     @Override
     public void onNodeRemoved(NodeRemoved arg0) {
         String s = "LOADBALANCER" + arg0.toString();
-        LOG.info(s);
+        LOG.info(TAG, s);
     }
 
 
@@ -144,7 +144,7 @@ public class LoadbalancerImpl implements BindingAwareProvider,
     @Override
     public void onNodeUpdated(NodeUpdated arg0) {
         String s = "LOADBALANCER" + arg0.toString();
-        LOG.info(s);
+        LOG.info(TAG, s);
     }
 
 
@@ -152,12 +152,12 @@ public class LoadbalancerImpl implements BindingAwareProvider,
     @Override
     public void onPacketReceived(PacketReceived arg0) {
         String s = "LOADBALANCER" + arg0.toString();
-        LOG.info(s);
+        LOG.info(TAG, s);
     }
 
 
 
-    private void testAccessDataInMdSal() {
+/*    private void testAccessDataInMdSal() {
         LOG.info("Test access data in MD-SAL - start");
         InstanceIdentifier<Topology> topologyInstanceIdentifier = InstanceIdentifier.builder(
                 NetworkTopology.class).child(
@@ -187,15 +187,13 @@ public class LoadbalancerImpl implements BindingAwareProvider,
             LOG.info(n.toString());
         }
         LOG.info("Test access data in MD-SAL - end");
-    }
+    }*/
 
 
 
 
     private void initLoadbalancerOperational() {
-        Loadbalancer loadbalancer = new LoadbalancerBuilder()
-                //.setDarknessFactor(1000l)
-                .setLoadbalancerStatus(LoadbalancerStatus.Down).build();
+        Loadbalancer loadbalancer = new LoadbalancerBuilder().setLoadbalancerStatus(LoadbalancerStatus.Down).build();
         WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
         tx.put(LogicalDatastoreType.OPERATIONAL, LOADBALANCER_IID, loadbalancer);
         Futures.addCallback(tx.submit(), new FutureCallback<Void>() {
@@ -223,81 +221,180 @@ public class LoadbalancerImpl implements BindingAwareProvider,
 
 
 
-    private void testSetRoleAction() {
-        LOG.info("Loadbalancer, RoleManager - start");
-        LOG.info("Loadbalancer, RoleManager - test BECOMESLAVE");
-        List<String> dpids = new ArrayList<String>();
-        dpids.add("1");
-        RoleUtil.fireRoleChange(2, dpids);
-        /**
-         * no change to role
-         *NOCHANGE(0),
-         * promote current role to MASTER
-         *BECOMEMASTER(1)
-         * demote current role to SLAVE
-         *BECOMESLAVE(2)
-         */
-
-        SessionManager sessionManager = OFSessionUtil.getSessionManager();
-        for (final SessionContext session : sessionManager.getAllSessions()) {
-            LOG.warn("WWWWWWWW", session.getRoleOnDevice().toString());
-            LOG.warn("WWWWWWWW", session.getRoleOnDevice().getIntValue());
-        }
-    }
-
-
 
 
 
     @Override
     public Future<RpcResult<StartLoadbalancerOutput>> startLoadbalancer(StartLoadbalancerInput input) {
-        LOG.info("Test start loadbalancer service - start");
-        // TESTING METHODS
-        testAccessDataInMdSal();
-        LOG.info("Test start loadbalancer service - end");
-        final SettableFuture<RpcResult<StartLoadbalancerOutput>> futureResult = SettableFuture.create();
-        //futureResult.set(1);
-        return futureResult;
+        LOG.info(TAG, "Starting LoadBalancer...");
+        LOG.info(TAG, "Write loadbalancer status in datastore");
+        Loadbalancer loadbalancer = new LoadbalancerBuilder().setLoadbalancerStatus(LoadbalancerStatus.Up).build();
+        WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
+        tx.put(LogicalDatastoreType.OPERATIONAL, LOADBALANCER_IID, loadbalancer);
+        Futures.addCallback(tx.submit(), new FutureCallback<Void>() {
+            @Override
+            public void onSuccess(final Void result) {
+            }
+            @Override
+            public void onFailure(final Throwable t) {
+            }
+        });
+        //
+        //TODO
+        //
+        //
+        //
+        StartLoadbalancerOutputBuilder slbob = new StartLoadbalancerOutputBuilder();
+        slbob.setResponseCode(1L);
+        slbob.setResponseMessage("LoadBalancer started");
+        LOG.info(TAG, "LoadBalancer started!");
+        return RpcResultBuilder.success(slbob.build()).buildFuture();
     }
+
+
+
+
+
+    @Override
+    public Future<RpcResult<GetLoadbalancerStatusOutput>> getLoadbalancerStatus() {
+        LOG.info(TAG, "Get LoadBalancer status started...");
+        LOG.info(TAG, "Reading LoadBalancer status");
+        ReadOnlyTransaction tx = dataBroker.newReadOnlyTransaction();
+        Optional<Loadbalancer> loadbalancer = null;
+        GetLoadbalancerStatusOutputBuilder glbsob = new GetLoadbalancerStatusOutputBuilder();
+        try {
+            loadbalancer = tx.read(LogicalDatastoreType.OPERATIONAL, LOADBALANCER_IID).get();
+        } catch (InterruptedException | ExecutionException e) {
+            LOG.error(TAG, "Error when retrieving the LoadBalancer status");
+            glbsob.setResponseCode(-1L);
+        }
+        if(loadbalancer!=null && loadbalancer.isPresent()){
+            LOG.error(TAG, "LoadBalancer status null or not present");
+            long status = loadbalancer.get().getLoadbalancerStatus().getIntValue();
+            glbsob.setResponseCode(status);
+        }
+        else
+            glbsob.setResponseCode(-1L);
+        LOG.info(TAG, "Returing LoadBalancer status");
+        return RpcResultBuilder.success(glbsob.build()).buildFuture();
+    }
+
+
+
 
 
 
     @Override
     public Future<RpcResult<StopLoadbalancerOutput>> stopLoadbalancer() {
-        // TODO Auto-generated method stub
-        return null;
+        LOG.info(TAG, "Stopping LoadBalancer...");
+        Loadbalancer loadbalancer = new LoadbalancerBuilder().setLoadbalancerStatus(LoadbalancerStatus.Down).build();
+        WriteTransaction tx = dataBroker.newWriteOnlyTransaction();
+        tx.put(LogicalDatastoreType.OPERATIONAL, LOADBALANCER_IID, loadbalancer);
+        Futures.addCallback(tx.submit(), new FutureCallback<Void>() {
+            @Override
+            public void onSuccess(final Void result) {
+            }
+            @Override
+            public void onFailure(final Throwable t) {
+            }
+        });
+        //
+        //TODO
+        //
+        //
+        //
+        StopLoadbalancerOutputBuilder slbob = new StopLoadbalancerOutputBuilder();
+        slbob.setResponseCode(1L);
+        LOG.info(TAG, "LoadBalancer stopped!");
+        return RpcResultBuilder.success(slbob.build()).buildFuture();
     }
 
 
 
+
     @Override
-    public Future<RpcResult<SetSwitchRoleOutput>> setSwitchRole(
-            SetSwitchRoleInput input) {
-        // TODO Auto-generated method stub
-        return null;
+    public Future<RpcResult<SetSwitchRoleOutput>> setSwitchRole(SetSwitchRoleInput input) {
+        LOG.info(TAG, "Set switches role stated");
+        //
+        //TODO input validation
+        //
+        //
+        int role = 0;
+        try{
+            role = Integer.parseInt(input.getOfpRole()+"");
+        }catch(RuntimeException e){
+            LOG.error(TAG, "Error while parsing the request's role");
+            SetSwitchRoleOutputBuilder swrob = new SetSwitchRoleOutputBuilder();
+            swrob.setResponseCode(-1L);
+            swrob.setResponseMessage("Error while parsing the request's role");
+            return RpcResultBuilder.success(swrob.build()).buildFuture();
+        }
+        /**
+         * no change to role
+         * NOCHANGE(0),
+         * promote current role to MASTER
+         * BECOMEMASTER(1)
+         * demote current role to SLAVE
+         * BECOMESLAVE(2)
+         */
+        if(role !=0 && input.getSwitchIds().size()>0){
+            RoleUtil.fireRoleChange(role, input.getSwitchIds());
+            SetSwitchRoleOutputBuilder swrob = new SetSwitchRoleOutputBuilder();
+            swrob.setResponseCode(0L);
+            swrob.setResponseMessage("Switch(es) role changed");
+            return RpcResultBuilder.success(swrob.build()).buildFuture();
+        }
+        else{
+            if(role==0){
+                LOG.warn(TAG, "Requested role change is 0 (NOCHANGE role), nothing to do...");
+                SetSwitchRoleOutputBuilder swrob = new SetSwitchRoleOutputBuilder();
+                swrob.setResponseCode(0L);
+                swrob.setResponseMessage("OK");
+                return RpcResultBuilder.success(swrob.build()).buildFuture();
+            }
+            else{
+                LOG.warn(TAG, "Requested role change is empty dpIDs list, nothing to do...");
+                SetSwitchRoleOutputBuilder swrob = new SetSwitchRoleOutputBuilder();
+                swrob.setResponseCode(0L);
+                swrob.setResponseMessage("OK");
+                return RpcResultBuilder.success(swrob.build()).buildFuture();
+            }
+        }
     }
 
 
 
     @Override
     public Future<RpcResult<GetSwitchRoleOutput>> getSwitchRole(GetSwitchRoleInput input) {
-        // TODO Auto-generated method stub
-        return null;
+        LOG.info(TAG, "Getting switches roles started");
+        List<String> dpRoles = new ArrayList<String>();
+        Map<String, String> swsRoles = RoleUtil.getSwitchesRoles();
+        if(swsRoles==null){
+            LOG.error(TAG, "Error while retieving the switches roles");
+            GetSwitchRoleOutputBuilder gsrob = new GetSwitchRoleOutputBuilder();
+            gsrob.setResponseCode(-1L);
+            gsrob.setResponseMessage(new ArrayList<String>());
+            return RpcResultBuilder.success(gsrob.build()).buildFuture();
+        }
+        for(String r : swsRoles.keySet()){
+            dpRoles.add(r.toString()+":"+getRoleIntValue(swsRoles.get(r)));
+        }
+        GetSwitchRoleOutputBuilder gsrob = new GetSwitchRoleOutputBuilder();
+        gsrob.setResponseCode(0L);
+        gsrob.setResponseMessage(dpRoles);
+        return RpcResultBuilder.success(gsrob.build()).buildFuture();
     }
 
 
 
-
-//    @Override
-//    public Future<RpcResult<Void>> setSwitchRole(SetSwitchRoleInput input) {
-//        LOG.info("LB_SATDATAPATH_ROLE_1: :", input.getOfpRole());
-//        //LOG.info("LB_SATDATAPATH_ROLE_2: :", input.getDatapathIds().get(0).toString());
-//        //LOG.info("LB_SATDATAPATH_ROLE_3: :", input.getDatapathIds().toArray().toString());
-//        testSetRoleAction();
-//        return null;
-//    }
-
-
-
+    private int getRoleIntValue(String string) {
+        // TODO
+        // TODO
+        // TODO
+        // 0 NOCHANGE
+        // 1 BECOME MASTAR
+        // 2 BECOME SLAVE
+        return 1;
+    }
 
 }
